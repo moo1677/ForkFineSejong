51ceea960f09746f40b62aa72cfae04ff0ea7816
moo1677 <ddonglgm@naver.com>
2025-05-16 17:11:53 +0900

    카테고리 버튼 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header.jsx";
-import MainBanner from "./components/MainBanner.jsx";
+import CategoryFilter from "./components/CategoryFilter.jsx";
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("전체");
   return (
     <>
       <Header onSearch={(text) => console.log("검색어:", text)} />
+      <CategoryFilter
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
     </>
   );
 }
